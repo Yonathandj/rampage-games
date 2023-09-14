a8484dc3b10dbe131f1175c8b4642ff3aeab563a
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-09-14 22:58:43 +0700

    Add Carousel Slider

--- frontend/src/components/Discover/Discover.jsx
+++ frontend/src/components/Discover/Discover.jsx
@@ -2,7 +2,7 @@ import Slider from "../Slider/Slider";
 
 const Discover = () => {
   return (
-    <div className="p-8 bg-gray-900">
+    <div className="p-10 bg-gray-900">
       <Slider />
     </div>
   );
--- frontend/src/components/Slider/Slider.jsx
+++ frontend/src/components/Slider/Slider.jsx
@@ -3,7 +3,11 @@ import axios from "axios";
 
 import { Carousel } from "flowbite-react";
 
+import "./Slider.css";
+
 const Slider = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
   const [games, setGames] = useState([]);
 
   useEffect(() => {
@@ -12,17 +16,35 @@ const Slider = () => {
         const response = await axios.get(
           `https://api.rawg.io/api/games?key=${
             import.meta.env.VITE_API_KEY
-          }&page_size=4`
+          }&page=5&page_size=4`
         );
         setGames(response.data.results);
       } catch (err) {
-        console.log(err);
+        setError(true);
+      } finally {
+        setLoading(false);
       }
     };
     getGames();
-    return setGames([]);
+    return setGames([]), setError(false);
   }, []);
-  return <Carousel></Carousel>;
+
+  return (
+    <Carousel className="mx-auto h-[500px] w-[800px]">
+      {loading || error ? (
+        <img src="https://placehold.co/600x400" alt="" />
+      ) : (
+        games.map((game) => (
+          <div key={game.id}>
+            <h2 className="font text-2xl p-2 font-semibold tracking-wide text-center text-slate-200">
+              {game.name}
+            </h2>
+            <img src={game.background_image} alt={game.name} />
+          </div>
+        ))
+      )}
+    </Carousel>
+  );
 };
 
 export default Slider;
