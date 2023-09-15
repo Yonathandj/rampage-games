cdc8217b40c46ef768bdc6341ec3d7ace4fd5b61
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-09-15 18:14:15 +0700

    Add Recommendation Section

--- frontend/src/components/Recommendation/Recommendation.jsx
+++ frontend/src/components/Recommendation/Recommendation.jsx
@@ -1,5 +1,30 @@
-const Recommendation = () => {
-  return <h2>Hello Fiony</h2>;
+import PropTypes from "prop-types";
+
+import RecommendationItem from "./RecommendationItem";
+import "./Recommendation.css";
+
+const Recommendation = ({ loading, error, games }) => {
+  const renderendItems = games.slice(4);
+  return (
+    <div className="py-8 px-24">
+      <section className="font text-center">
+        <h2 className="text-lg font-medium text-slate-200">
+          Recommendation Games
+        </h2>
+      </section>
+      <RecommendationItem
+        loading={loading}
+        error={error}
+        games={renderendItems}
+      />
+    </div>
+  );
+};
+
+Recommendation.propTypes = {
+  loading: PropTypes.bool,
+  error: PropTypes.bool,
+  games: PropTypes.array,
 };
 
 export default Recommendation;
